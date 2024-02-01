dc40c7b155d3c70e19e75fdebf7468aa6888fff2
test231212 <j910219@gmail.com>
2024-02-01 16:50:00 +0900

    페이징 완료

--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -1,23 +1,29 @@
 package shop.mtcoding.blog.board;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import shop.mtcoding.blog._core.Constant;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import java.util.List;
-
 @RequiredArgsConstructor
 @Repository
 public class BoardRepository {
     private final EntityManager em;
+    private final JdbcTemplate jdbcTemplate;
+
+    public int count(){
+
+        return 1;
+    }
 
     public List<Board> findAll(int page){
-        final int COUNT = 3;
-        int value = page*COUNT;
+        int value = page* Constant.PAGING_COUNT;
         Query query = em.createNativeQuery("select * from board_tb order by id desc limit ?,?", Board.class);
         query.setParameter(1, value);
-        query.setParameter(2, COUNT);
+        query.setParameter(2, Constant.PAGING_COUNT);
 
         List<Board> boardList = query.getResultList();
         return boardList;

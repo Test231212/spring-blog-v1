246a8d44b90fcb8dbf423d2122247ca087fea801
test231212 <j910219@gmail.com>
2024-02-01 14:34:03 +0900

    게시글 목록보기 페이징중

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -3,25 +3,49 @@ package shop.mtcoding.blog.board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import shop.mtcoding.blog.user.User;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
 
     private final HttpSession session;
+    private final BoardRepository boardRepository;
 
+    // http://localhost:8080?page=0
     @GetMapping({ "/", "/board" })
-    public String index() {
+    public String index(HttpServletRequest request, @RequestParam(defaultValue = "0") int page) {
+        List<Board> boardList = boardRepository.findAll(page);
+        request.setAttribute("boardList", boardList);
 
-//        User sessionUser = (User) session.getAttribute("sessionUser");
-//        if(sessionUser == null){
-//            System.out.println("로그인 안된 상태입니다");
-//        }else{
-//            System.out.println("로그인 된 상태입니다");
-//        }
+        int currentPage = page;
+        int nextPage = currentPage+1;
+        int prevPage = currentPage-1;
+        request.setAttribute("nextPage", nextPage);
+        request.setAttribute("prevPage", prevPage);
+
+        boolean first = currentPage == 0 ? true : false;
+
+        int totalCount = 7;
+
+        boolean last = true;
+
+        // t = 7, cp = 0, last = false
+        // t = 7, cp = 1, last = false
+        // t = 7, cp = 2, last = true
+
+        request.setAttribute("first", first);
 
         return "index";
     }
